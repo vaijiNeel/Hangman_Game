26584832db6ca1a587a676e8c7540be26fe3f692
Vaijayanthi Neelakandan <nvaij@yahoo.com>
2017-07-10 15:53:40 -0400

    updated files

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,7 @@
 
 var winCount=0, lostCount=0, guessCount=0, totalGuess=15, numOfWords=4, n=0;
-var wordsToGuess = ["gryffindor", "slytherin", "rawenclaw", "hufflepuff"]
+var remainGuesses;
+var wordsToGuess = ["gryffindor", "slytherin", "ravenclaw", "hufflepuff"]
 var lostLetters = [], guessedLetters = [];
 
 var hogHouses = {
@@ -13,22 +14,13 @@ var hogHouses = {
 		var word="";
 		for (var i = 0; i < arr.length; i++) {
 			word+=arr[i];
-		}
-		// 	document.querySelector("#curWord").innerHTML = word;
-		// 	document.getElementById("#guessedWrd").innerHTML = word;
+		}		
 		document.getElementById(idName).innerHTML = word;
 	},
 
-	winCountFn: function() {
-		winCount++;
-	},
-
-	guessCountFn: function() {
-		guessCount++;
-		if (guessCount==totalGuess) {
-			lostCount++;
-			//display next word
-		}
+	changeHouseEmblem: function(hdr, imgName) {
+		document.getElementById("gameHdr3").innerHTML = hdr;				
+		document.getElementById("imgHogwarts").src="assets/images/"+imgName;
 	}
 };
 
@@ -37,18 +29,68 @@ function resetNextWord() {
 	n++;
 	if(n===1) {
 		guessedLetters = hogHouses.sPlaceHolder;
+		hogHouses.printLetters(guessedLetters, "curWordId");
 	}
 	else if(n===2) {
 		guessedLetters = hogHouses.rPlaceHolder;
+		hogHouses.printLetters(guessedLetters, "curWordId");
 	}
 	else if(n===3) {
 		guessedLetters = hogHouses.hPlaceHolder;
+		hogHouses.printLetters(guessedLetters, "curWordId");
 	}
-	hogHouses.printLetters(guessedLetters, "curWordId");
+	else if(n===4) {
+		document.getElementById("curWordId").innerHTML = "";
+		document.getElementById("remainGuessId").innerHTML = "";
+	}	
 	// reset variables
 	lostLetters = [];
 	hogHouses.printLetters(lostLetters, "lostWrdId");
 	guessCount=0;
+}
+
+function endWordOrGuessCount() {	
+	// check guess count reached max
+	if (remainGuesses===0) {
+			lostCount++;
+			document.getElementById("loseCntId").innerHTML = lostCount;
+			document.getElementById("remainGuessId").innerHTML = "";
+			resetNextWord();
+	}
+	else {
+		// check whether all the letters are guessed
+		var wordGuessed=false;
+		for (var i = 0; i < guessedLetters.length; i++) {
+			if(guessedLetters[i]==="_") {
+				wordGuessed=false;
+				break;
+			}
+			else
+				wordGuessed=true;
+		}
+		if(wordGuessed) {
+			winCount++;
+			document.getElementById("winCntId").innerHTML = winCount;
+			// change house emblem
+			if(n===0) {
+				hogHouses.changeHouseEmblem("Gryffindor","gryffindor.jpg");
+			}
+			else if(n===1) {
+				hogHouses.changeHouseEmblem("Slytherin","slytherin.jpg");
+			}
+			else if(n===2) {
+				hogHouses.changeHouseEmblem("Rawenclaw","rawenclaw.jpg");
+			}
+			else if(n===3) {
+				hogHouses.changeHouseEmblem("Hufflepuff","hufflepuff.jpg");
+			}
+			document.getElementById("remainGuessId").innerHTML = "";
+			//display sound
+			var audio = new Audio("../Hangman_Game/assets/images/brilliant.mp3");
+			audio.play();
+			resetNextWord();				
+		}
+	}
 }
 
 function evaluateLetter(userGuess, m) {
@@ -57,89 +99,81 @@ function evaluateLetter(userGuess, m) {
 	asciiChar = userGuess.charCodeAt(0);
 	// check if the typed letter is alphabet
 	if(asciiChar>=97 && asciiChar<=122) {
-		guessCount++;
-		// check guess count reached max
-		if (guessCount>totalGuess) {
-			lostCount++;
-			document.getElementById("loseCntId").innerHTML = lostCount;
-			resetNextWord();
+		var alreadyExists=false;
+		for (var i = 0; i < guessedLetters.length; i++) {
+			if (guessedLetters[i]===userGuess) {
+				alreadyExists=true;
+				break;
+			}
 		}
-		// if guess count is not max
-		else {
-			var word = wordsToGuess[m];
-			console.log(word);
-			var wordGuessed=false;
-			// check whether all the letters are guessed
-			for (var i = 0; i < guessedLetters.length; i++) {
-				if(guessedLetters[i]==="_") {
-					wordGuessed=false;
+		if(!alreadyExists) {
+			for (var i = 0; i < lostLetters.length; i++) {
+				if (lostLetters[i]===userGuess) {
+					alreadyExists=true;
 					break;
 				}
-				else
-					wordGuessed=true;
 			}
-			if(wordGuessed) {
-				winCount++;
-				document.getElementById("winCntId").innerHTML = winCount;
-				//display sound
-				// var audio = new Audio('assets/images/brilliant.wav');
-				// audio.play();
-				resetNextWord();				
+		}
+		if(!alreadyExists) {
+			guessCount++;		
+			var word = wordsToGuess[m];
+			console.log(word);							
+			// find whether the letter typed is in the word
+			var indices = [];
+			for(var i=0; i<word.length;i++) {
+			    if (word[i] === userGuess) {
+			    	indices.push(i);
+			    }
 			}
-			// if still more letters to be guessed
-			else {				
-				// find whether the letter typed is in the word
-				var indices = [];
-				for(var i=0; i<word.length;i++) {
-				    if (word[i] === userGuess) {
-				    	indices.push(i);
-				    }
+			console.log(indices.length);
+			// if the letter is present display in current word
+			if(indices.length > 0) {
+				for (var i = 0; i < indices.length; i++) {
+					var gIndex = indices[i];
+					guessedLetters[gIndex] = userGuess;
 				}
-				console.log(indices.length);
-				// if the letter is present display in current word
-				if(indices.length > 0) {
-					for (var i = 0; i < indices.length; i++) {
-						var gIndex = indices[i];
-						guessedLetters[gIndex] = userGuess;
+				hogHouses.printLetters(guessedLetters, "curWordId");
+			}
+			// if the letter is not present display in lost letters list
+			else if(indices.length == 0) {	
+				var flag=false
+				for (var i = 0; i < lostLetters.length; i++) {
+					if(lostLetters[i]===userGuess) {
+						flag=true;
+						break;
 					}
-					hogHouses.printLetters(guessedLetters, "curWordId");
-				}
-				// if the letter is not present display in lost letters list
-				else if(indices.length == 0) {	
-					var flag=false
-					for (var i = 0; i < lostLetters.length; i++) {
-						if(lostLetters[i]===userGuess) {
-							flag=true;
-							break;
-						}
-						else
-							flag=false;
-					}	
-					if(!flag)
-						lostLetters.push(userGuess);
-					hogHouses.printLetters(lostLetters, "lostWrdId");
-					console.log(lostLetters);
+					else
+						flag=false;
 				}	
-				// display remaining guess count				
-				var remainGuesses = totalGuess - guessCount;
-				document.getElementById("remainGuessId").innerHTML = remainGuesses;
-			}
+				if(!flag)
+					lostLetters.push(userGuess);
+				hogHouses.printLetters(lostLetters, "lostWrdId");
+				console.log(lostLetters);
+			}	
+			// display remaining guess count				
+			remainGuesses = totalGuess - guessCount;
+			document.getElementById("remainGuessId").innerHTML = remainGuesses;	
+			endWordOrGuessCount();	
 		}
 	}
 }
 
-// gryffindor word
+hogHouses.changeHouseEmblem("Guess Hogwarts House","hogwarts.jpg");
+// set gryffindor word
 if(n===0) {
 	guessedLetters = hogHouses.gPlaceHolder;
-	hogHouses.printLetters(guessedLetters, "curWordId");
 }
-
-// first key press to invoke the game
-// document.onkeypress = function(event) {
-// 	hogHouses.printLetters(hogHouses.gPlaceHolder, "curWord");	
-// }
-
+//main
+var eventFlag=true;
 document.onkeyup = function(event) {
 	var userGuess = event.key;
-	evaluateLetter(userGuess,n);
+	// set flag for first event to start the game
+	if(eventFlag) {
+		hogHouses.printLetters(guessedLetters, "curWordId");
+		eventFlag=false;
+	}
+	else {
+		if(n<4)
+			evaluateLetter(userGuess,n);
+	}
 }
